5af2015a9f599914307c3d6014c45b2cd0937042
Junior <dirceusilva0422@gmail.com>
2022-08-18 13:15:31 -0300

    fix: main menu js fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App"
 const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
