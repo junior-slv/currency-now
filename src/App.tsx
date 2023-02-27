131d85ad63ef90d7df92ef296d0094d3baf52fce
junior-slv <dirceusilva0422@gmail.com>
2023-02-27 00:43:53 -0300

    added initial files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Currency from './components/Currency/Currency';
+function App() {
+
+  console.log(process.env)
+  return (
+    <>
+    <Currency/>
+      </>
+  );
+}
+
+export default App;
