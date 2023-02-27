4e73a8cc0b330404a3c42cf160257b50d96e0ab7
junior-slv <dirceusilva0422@gmail.com>
2023-02-27 01:31:47 -0300

    added get from currency api

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+
+import { useEffect, useState } from 'react'
 import './Currency.css'
+import axios from 'axios'
 
 
 const Currency = () => {
+  const [firstValue, setFirstValue] = useState("USD")
+  const [secondValue, setSecondValue] = useState("BRL")
+  const [current, setCurrent] = useState("")
+  const [multiplier, setMultiplier] = useState("")
+  const [finalCurrent, setFinalCurrent] = useState("")
+
+  const US = "USDBRL"
+
+
+  const api = `https://economia.awesomeapi.com.br/last/${firstValue}-${secondValue}`
+
+
+  useEffect(() => {
+    axios
+      .get(api)
+      .then((response) => setMultiplier(response.data))
+      .catch((err) => {
+        console.error("Error" + err);
+        
+      });
+  }, []);
+
+  //ADICIONAR A VARIAVEL HIGH NA MULTIPLICAÇÃO
+  console.log(  Object.keys(multiplier).map )
+  console.log(multiplier);
+    
   return (
-    <div>Currency</div>
+
+    
+
+
+    <div>{JSON.stringify(multiplier)}</div>
   )
 }
 
