31eb03f38076664f7ba7e9aa8ecef190f88bc101
junior-slv <dirceu.junior@edu.unifil.br>
2023-02-28 17:56:32 -0300

    fixed functions

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,8 +1,8 @@
 
-import { SetStateAction, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './Currency.css'
 import axios from 'axios'
-import { type } from 'os'
+
 
 type CurrencyQuote = {
   ask: string,
@@ -28,7 +28,7 @@ const Currency = () => {
   const api = `https://economia.awesomeapi.com.br/last/${firstValue}-${secondValue}`
 
   const idref = useRef(null);
-
+  let multi:any;
 
 
 
@@ -39,16 +39,20 @@ const Currency = () => {
       .catch((err) => {
         console.error("Error" + err);
       });
-
+      let multi:any = currencyQuote?.high
+      multi = parseFloat(multi)
   }, []);
-  let multi:any = currencyQuote?.high
-  multi = parseFloat(multi)
+
 
   const handleChange = (event: any) =>{
     setCurrent(parseInt(event.target.value))
   }
   const handleFinal = () =>{
+
+    console.log(firstValue);
+    console.log(secondValue);
     setFinalCurrent(current*multi)
+
   }
 
   return (
@@ -60,25 +64,28 @@ const Currency = () => {
           </div>
           <div className="main-screen-middle df">
             <label htmlFor="">Insert a value</label>
-            <input  onChange={handleChange} type="number" name="" id=""  />
+            <input  onClick={handleChange} type="number" name="" id=""  />
             <input  onClick={handleFinal} type="button" value="ENVIAR" />
           </div>
           <div className="main-screen-bottom">
             <div className="main-screen-bottom-left">
-              <label htmlFor="cars">Choose a currency:</label>
-                <select id="cars" name="cars">
-                  <option ref={idref} id="brl">BRL - Real</option>
-                  <option id="usd">USD - Dolar</option>
-                  <option id="eur">EUR - Euro</option>
+              
+              <label htmlFor="currency">Choose a currency:</label>
+                <select id="currency" name="currency">
+                  <option >BRL - Real</option> s
+                  <option >USD - Dolar</option>
+                  <option >EUR - Euro</option>
                 </select>
-                <i className='bx bx-right-arrow-alt'></i>
+            <span>
+              <i className='bx bx-right-arrow-alt'></i>
+            </span>
             </div>
             <div className="main-screen-bottom-right">
               <label htmlFor="cars">Choose a currency:</label>
                 <select id="cars" name="cars">
-                  <option value="brl">BRL - Real</option>
-                  <option value="usd">USD - Dolar</option>
-                  <option value="eur">EUR - Euro</option>
+                  <option onClick={() => setSecondValue("BRL")}>BRL - Real</option>
+                  <option onClick={() => setSecondValue("USD")}>USD - Dolar</option>
+                  <option onClick={() => setSecondValue("EUR")}>EUR - Euro</option>
                 </select>
 
             </div>
