04ec6153e4a7f4324cfbdbe4cec6b02aa959dd5b
junior-slv <geovanaandrade@edu.unifil.br>
2023-02-28 15:19:25 -0300

    fixed current function

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { SetStateAction, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './Currency.css'
 import axios from 'axios'
 
@@ -19,13 +19,15 @@ type CurrencyQuote = {
 const Currency = () => {
   const [firstValue, setFirstValue] = useState("USD")
   const [secondValue, setSecondValue] = useState("BRL")
-  const [current, setCurrent] = useState(0.0)
+  const [current, setCurrent] = useState(0)
   const [finalCurrent, setFinalCurrent] = useState(0)
   const [currencyQuote, setCurrencyQuote] = useState<null | CurrencyQuote>(null)
+  const [multiplier, setMultiplier] = useState(0)
 
   const api = `https://economia.awesomeapi.com.br/last/${firstValue}-${secondValue}`
 
   const idref = useRef(null);
+
 
 
 
@@ -35,30 +37,38 @@ const Currency = () => {
       .then((response) => setCurrencyQuote(Object.values(response.data)[0] as CurrencyQuote))
       .catch((err) => {
         console.error("Error" + err);
-        
       });
-      //ESTUDAR SOBRE USE REF 
+      let multi:any = currencyQuote?.high
+      multi = parseFloat(multi)
+      setMultiplier(multi)  
   }, []);
-  let multiplier:any = currencyQuote?.high
-  multiplier = parseFloat(multiplier)
+
+  const handleChange = () =>{
+    console.log(current)
+  }
+
 
   return (
     <div className="container">
-        <div className="main-screen">
-          <div className="main-screen-top">
-            <h2>Conversor de moeda</h2>
-            <p>Insert value:</p>
-            <input type="text" name="" id="" />
+        <div className="main-screen_ df">
+          <div className="main-screen-top df">
+            <h2>Currency converter</h2>
+
+          </div>
+          <div className="main-screen-middle df">
+            <label htmlFor="">Insert a value</label>
+            <input  onClick={() => handleChange} type="number" name="" id=""  />
+            <input  type="button" value="ENVIAR" />
           </div>
           <div className="main-screen-bottom">
             <div className="main-screen-bottom-left">
               <label htmlFor="cars">Choose a currency:</label>
-                <select  onChange={() => { setFirstValue("");
-                }} id="cars" name="cars">
+                <select id="cars" name="cars">
                   <option ref={idref} id="brl">BRL - Real</option>
                   <option id="usd">USD - Dolar</option>
                   <option id="eur">EUR - Euro</option>
                 </select>
+                <i className='bx bx-right-arrow-alt'></i>
             </div>
             <div className="main-screen-bottom-right">
               <label htmlFor="cars">Choose a currency:</label>
@@ -67,9 +77,11 @@ const Currency = () => {
                   <option value="usd">USD - Dolar</option>
                   <option value="eur">EUR - Euro</option>
                 </select>
-              <p>Final value: {finalCurrent}</p>
+
             </div>
+
           </div>
+          <span><p>Final value: {finalCurrent}</p></span>
         </div>
 
     </div>
