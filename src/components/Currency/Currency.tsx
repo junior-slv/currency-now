c9f5ee8db9ba49e5c6e165bcb5985cb5507aae63
junior-slv <dirceu.junior@edu.unifil.br>
2023-03-02 16:50:16 -0300

    fixed convert function with values

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,119 +1,121 @@
-
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import './Currency.css'
-import axios from 'axios'
-
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "./Currency.css";
+import axios from "axios";
 
 type CurrencyQuote = {
-  ask: string,
-  bid: string,
-  code: string,
-  codein: string,
-  high: number,
-  low: number,
-  varBid: string,
-  pctChange: string,
-  timestamp: string,
-  create_dat: string
-}
+  ask: string;
+  bid: string;
+  code: string;
+  codein: string;
+  high: number;
+  low: number;
+  varBid: string;
+  pctChange: string;
+  timestamp: string;
+  create_dat: string;
+};
 
 const Currency = () => {
-  const [firstValue, setFirstValue] = useState("USD")
-  const [secondValue, setSecondValue] = useState("BRL")
-  const [current, setCurrent] = useState(0)
-  const [finalCurrent, setFinalCurrent] = useState(0)
-  const [currencyQuote, setCurrencyQuote] = useState<null | CurrencyQuote>(null)
-  const [multiplier, setMultiplier] = useState<null | undefined>(null)
+  const [firstValue, setFirstValue] = useState("BRL");
+  const [secondValue, setSecondValue] = useState("USD");
+  const [current, setCurrent] = useState(0);
+  const [finalCurrent, setFinalCurrent] = useState(0);
+  const [currencyQuote, setCurrencyQuote] = useState<null | CurrencyQuote>(
+    null
+  );
+  const [multiplier, setMultiplier] = useState(0);
 
-  const api = `https://economia.awesomeapi.com.br/last/${firstValue}-${secondValue}`
-
-  const idref = useRef(null);
-
-
-
+  const api = `https://economia.awesomeapi.com.br/last/${firstValue}-${secondValue}`;
 
   useEffect(() => {
     axios
       .get(api)
-      .then((response) => setCurrencyQuote(Object.values(response.data)[0] as CurrencyQuote))
+      .then((response) =>
+        setCurrencyQuote(Object.values(response.data)[0] as CurrencyQuote)
+      )
       .catch((err) => {
         console.error("Error" + err);
       });
-
   }, []);
-  setMultiplier(currencyQuote?.high)
-  let multi:any = currencyQuote?.high
-  multi = parseFloat(multi)
-  const handleChange = (event: any) =>{
-    setCurrent(parseInt(event.target.value))
-  }
 
+  let multi: any = currencyQuote?.high;
+  multi = parseFloat(multi);
+  const handleChange = (event: any) => {
+    setCurrent(parseInt(event.target.value));
+    console.log(current);
+  };
 
-
-
-
-  const handleFinal = () =>{
+  const handleFinal = () => {
     axios
       .get(api)
-      .then((response) => setCurrencyQuote(Object.values(response.data)[0] as CurrencyQuote))
+      .then((response) =>
+        setCurrencyQuote(Object.values(response.data)[0] as CurrencyQuote)
+      )
       .catch((err) => {
         console.error("Error" + err);
       });
-    let multi:any = currencyQuote?.high
-    multi = parseFloat(multi)  
-    setFinalCurrent(current*multi)
-    console.log(typeof(finalCurrent));
-    
-    console.log(typeof(current));
-    console.log(typeof(multi));
-    console.log(finalCurrent);
-    
-    
-    
+    if(firstValue === secondValue){
+      setFinalCurrent(current)
+    }else{
+    let multi: any = currencyQuote?.high;
+    multi = parseFloat(multi);
 
+    setMultiplier(parseFloat(multi));
+    setFinalCurrent(current * multiplier);
+
+    console.log(typeof finalCurrent);
+    console.log(typeof current);
+    console.log(typeof multi);
+    console.log(finalCurrent);
+    console.log(firstValue);
+    console.log(secondValue);
   }
+  };
 
   return (
     <div className="container">
-        <div className="main-screen_ df">
-          <div className="main-screen-top df">
-            <h2>Currency converter</h2>
-
-          </div>
-          <div className="main-screen-middle df">
-            <label htmlFor="">Insert a value</label>
-            <input  onClick={handleChange} type="number" name="" id=""  />
-            <input  onClick={handleFinal} type="button" value="ENVIAR" />
-          </div>
-          <div className="main-screen-bottom">
-            <div className="main-screen-bottom-left">
-              
-              <label htmlFor="currency">Choose a currency:</label>
-                <select id="currency" name="currency">
-                  <option >BRL - Real</option> s
-                  <option >USD - Dolar</option>
-                  <option >EUR - Euro</option>
-                </select>
-            <span>
-              <i className='bx bx-right-arrow-alt'></i>
-            </span>
-            </div>
-            <div className="main-screen-bottom-right">
-              <label htmlFor="cars">Choose a currency:</label>
-                <select id="cars" name="cars">
-                  <option onClick={() => setSecondValue("BRL")}>BRL - Real</option>
-                  <option onClick={() => setSecondValue("USD")}>USD - Dolar</option>
-                  <option onClick={() => setSecondValue("EUR")}>EUR - Euro</option>
-                </select>
-
-            </div>
-
-          </div>
-          <span><p>Final value: {finalCurrent}</p></span>
+      <div className="main-screen_ df">
+        <div className="main-screen-top df">
+          <h2>Currency converter</h2>
         </div>
-
+        <div className="main-screen-middle df">
+          <label htmlFor="">Insert a value</label>
+          <input onChange={handleChange} type="number" name="" id="" />
+          <input onClick={handleFinal} type="button" value="ENVIAR" />
+        </div>
+        <div className="main-screen-bottom">
+          <div className="main-screen-bottom-left">
+            <label htmlFor="currency">Choose a currency:</label>
+            <select
+              value={firstValue}
+              onChange={(event) => setFirstValue(event.target.value)}
+            >
+              <option value="BRL">BRL - Real</option>
+              <option value="USD">USD - Dolar</option>
+              <option value="EUR">EUR - Euro</option>
+            </select>
+            <span>
+              <i className="bx bx-right-arrow-alt"></i>
+            </span>
+          </div>
+          <div className="main-screen-bottom-right">
+            <label htmlFor="cars">Choose a currency:</label>
+            <select
+              value={secondValue}
+              onChange={(event) => setSecondValue(event.target.value)}
+            >
+              <option value="BRL">BRL - Real</option>
+              <option value="USD">USD - Dolar</option>
+              <option value="EUR">EUR - Euro</option>
+            </select>
+          </div>
+        </div>
+        <span>
+          <p>Final value: {finalCurrent.toFixed(2)}</p>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Currency
+export default Currency;
